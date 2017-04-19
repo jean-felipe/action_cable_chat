b9a946c99cc3b10689f6eb89b156a1ca229d7bed
jean-felipe <mandakeru@gmail.com>
2017-04-18 23:03:58 -0300

    second changes

--- db/migrate/20170416224332_create_messages.rb
+++ db/migrate/20170416224332_create_messages.rb
@@ -3,7 +3,7 @@ class CreateMessages < ActiveRecord::Migration[5.0]
     create_table :messages do |t|
       t.references :chatroom, foreign_key: true
       t.references :user, foreign_key: true
-      t.text :body
+      t.string :body
 
       t.timestamps
     end
