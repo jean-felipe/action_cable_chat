e8f09cc74798b8be2a57733efb6e460a27f6dd7b
jean-felipe <jean-felipeds@hotmail.com>
2017-04-21 02:00:38 -0300

    Revert "list of online users working"

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -17,7 +17,6 @@
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #  dialects               :integer
-#  online                 :boolean          default("false")
 #
 # Indexes
 #
