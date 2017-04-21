11a43d3d1fd640b48177efee9d2edcb9ac0b2a0d
jean-felipe <mandakeru@gmail.com>
2017-04-21 00:20:12 -0300

    translation API working good

--- app/jobs/message_relay_job.rb
+++ app/jobs/message_relay_job.rb
@@ -1,10 +1,29 @@
+require 'net/http'
+require 'uri'
+require 'json'
+require 'nokogiri'
+require 'open-uri'
+
 class MessageRelayJob < ApplicationJob
   queue_as :default
 
   def perform(message)
+  	translation_call(message)
     ActionCable.server.broadcast "chatrooms:#{message.chatroom.id}", {
       message: MessagesController.render(message),
       chatroom_id: message.chatroom.id
   }
+  end
+
+  private
+
+  def translation_call(message)
+  	uri = URI.parse("http://www.degraeve.com/cgi-bin/babel.cgi?d=#{message.user.dialects}&url=&w=#{message.body}")
+  	response = Net::HTTP.get(uri)
+
+  	page = Nokogiri::HTML(response)
+  	
+  	message.body = page.css("p").text
+  	return message
   end
 end
